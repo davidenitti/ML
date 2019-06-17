559188d48498d8ea865ed1937dd18df1fcca0aff
davide <davidenitti@gmail.com>
2019-06-17 22:50:43 +0200

    dummy commit

--- classification/train_cifar10.py
+++ classification/train_cifar10.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import argparse
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
